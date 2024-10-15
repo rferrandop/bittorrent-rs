39e3128d7c303490dc5b549bde395a66565acc9c
Raul Ferrando <raulferrando@protonmail.com>
2024-10-15 19:03:31 +0200

    first stage: uncomment block

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use serde_json;
 use std::env;
 
 // Available if you need it!
@@ -13,7 +12,8 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+
+        serde_json::Value::String(string.to_string())
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -28,10 +28,9 @@ fn main() {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         println!("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
